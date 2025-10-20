8f1395a5aa1ffb6e6d69102aec52de407e5550ec
Lil Ameer <myuguda175@gmail.com>
2025-10-20 13:57:32 +0400

    latest updates

---
+++ products.js
@@ -0,0 +1,30 @@
+let products = [
+{
+                        'id': 1001,
+                        'title': "Cat Food, 28lb bag.",
+                        'description': "The best and healthy cat food for your cat.",
+                        'price': 5000,
+                        'image':'Image/cat.png',
+                        'availableInventory': 10,
+                        'rating': 3
+},
+{
+    'id': 1002,
+    'title': "Chicken Feed,",
+    'description': "The best and healthy cat food for your cat.",
+    'price': 2000,
+    'image':'Image/img3.jpg',
+    'availableInventory': 7,
+    'rating': 4.5
+},
+{
+    'id': 1003,
+    'title': "Dog Food, 13lb bag.",
+    'description': "The best and healthy cat food for your cat.",
+    'price': 1000,
+    'image':'Image/img2.png',
+    'availableInventory': 6,
+    'rating': 5
+},
+
+]
